c3504af03eccac12b545509b4b9d51e5f8a9653a
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-09-15 11:05:56 +0530

    Homepage layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Routes>
-        <Route path="/" element={<MenuList />} />
-        <Route path="/product/:productId" element={<ProductModal />} />
-      </Routes>
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<MenuList />} />
+          <Route path="/product/:productId" element={<ProductModal />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
